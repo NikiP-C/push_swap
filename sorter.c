458039fac48f7f37790f97b11eb10e5ed6b03491
Niki Philippart <nphilipp@f0r6s14.codam.nl>
2021-03-08 19:33:53 +0100

    first commit

---
+++ sorter.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   sorter.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nphilipp <nphilipp@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/03/08 19:08:39 by nphilipp      #+#    #+#                 */
+/*   Updated: 2021/03/08 19:30:44 by nphilipp      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "CodamGNL/get_next_line.h"
+
+int	check(char *str)
+{
+	if (strcmp)
+}
+
+int	main(int ac, char **av)
+{
+	char	*str;
+	int i;
+
+	i = 1;
+	if (ac == 1)
+		return (0);
+	while (i == 1)
+	{
+		i = get_next_line(0, &str);
+		check(str);
+	}
+}
