6090e2dc3f1ecc6f331974926dbb050188c50bbb
Niki Philippart <nphilipp@f0r5s17.codam.nl>
2021-03-22 19:09:53 +0100

    header change

--- checker.h
+++ checker.h
@@ -6,12 +6,13 @@
 /*   By: nphilipp <nphilipp@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/22 15:14:22 by nphilipp      #+#    #+#                 */
-/*   Updated: 2021/03/22 15:15:32 by nphilipp      ########   odam.nl         */
+/*   Updated: 2021/03/22 19:09:39 by nphilipp      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKER_H
 # define CHECKER_H
 # include "libft/libft.h"
+
 
 #endif
